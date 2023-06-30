6f44324248ae155cf856cde9061b338a9737f5ad
Doguhan Akgunes <137198411+dovuhan@users.noreply.github.com>
2023-06-30 17:03:31 +0300

    Update native.node.plugin.js
    The build.onResolve callback for resolving ".node" files in the "file" namespace has been simplified. It now directly returns the resolved path in the "node-file" namespace.
    
    The build.onLoad callback for loading ".node" files from the "node-file" namespace has been simplified. The unnecessary check for the file extension has been removed, and the contents property is updated to include the imported path module correctly.
    
    The build.onResolve callback for putting ".node" files back into the "file" namespace has been simplified. It now directly returns the path in the "file" namespace.
    
    The code for configuring the loader for ".node" files has been simplified. It checks if opts.loader exists before assigning the ".node" file loader.

--- opencti-platform/opencti-graphql/builder/plugin/native.node.plugin.js
+++ opencti-platform/opencti-graphql/builder/plugin/native.node.plugin.js
@@ -3,39 +3,30 @@ const path = require('node:path');
 const nativeNodeModulesPlugin = () => ({
   name: 'native-node-modules',
   setup(build) {
-    // If a ".node" file is imported within a module in the "file" namespace, resolve
-    // it to an absolute path and put it into the "node-file" virtual namespace.
+    // Resolve ".node" files and put them in the "node-file" virtual namespace
     build.onResolve({ filter: /\.node$/, namespace: 'file' }, (args) => {
       const resolvedId = require.resolve(args.path, {
         paths: [args.resolveDir],
       });
-      if (resolvedId.endsWith('.node')) {
-        return {
-          path: resolvedId,
-          namespace: 'node-file',
-        };
-      }
       return {
         path: resolvedId,
+        namespace: 'node-file',
       };
     });
 
-    // Files in the "node-file" virtual namespace call "require()" on the
-    // path from esbuild of the ".node" file in the output directory.
+    // Load ".node" files from the "node-file" virtual namespace
     build.onLoad({ filter: /.*/, namespace: 'node-file' }, (args) => {
       return {
         contents: `
-            import path from ${JSON.stringify(args.path)}
-            try { module.exports = require(path) }
-            catch {}
-          `,
+          import path from ${JSON.stringify(args.path)};
+          try { module.exports = require(path); }
+          catch {}
+        `,
         resolveDir: path.dirname(args.path),
       };
     });
 
-    // If a ".node" file is imported within a module in the "node-file" namespace, put
-    // it in the "file" namespace where esbuild's default loading behavior will handle
-    // it. It is already an absolute path since we resolved it to one above.
+    // Put ".node" files back into the "file" namespace
     build.onResolve(
       { filter: /\.node$/, namespace: 'node-file' },
       (args) => ({
@@ -44,8 +35,7 @@ const nativeNodeModulesPlugin = () => ({
       })
     );
 
-    // Tell esbuild's default loading behavior to use the "file" loader for
-    // these ".node" files.
+    // Use the "file" loader for ".node" files
     const opts = build.initialOptions;
     opts.loader = opts.loader || {};
     opts.loader['.node'] = 'file';
